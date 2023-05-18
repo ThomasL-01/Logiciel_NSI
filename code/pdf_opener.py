b2855273e8b34516478c57204066ecc3f8994697
Thomas.L <90705601+ThomasL1G@users.noreply.github.com>
2023-05-18 11:27:10 +0200

    Mise à jour et organisation des fichiers

--- code/pdf_opener.py
+++ code/pdf_opener.py
@@ -34,23 +34,35 @@ def recup_path() -> str:
     file_path = askopenfilename(filetypes=[("Fichiers PDF", "*.pdf")])
     return file_path
 
-def copy_file(file_path: str) -> str:
-    """Copie le fichier donné dans le répertoire PDF"""
-    cwd = os.getcwd()
-
-    target_folder = os.path.join(cwd, '../PDF')
-
+def copy_file(file_path):
+    # Obtenir le chemin absolu du répertoire parent du fichier actuel
+    parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    
+    # Créer un dossier "Copies" dans le répertoire parent s'il n'existe pas déjà
+    target_folder = os.path.join(parent_dir, "PDF")
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+    
+    # Obtenir le nom de fichier à partir du chemin absolu
+    filename = os.path.basename(file_path)
+    
+    # Construire le chemin de destination du fichier copié
+    destination = os.path.join(target_folder, filename)
+    
+    # Vérifier si le fichier existe déjà dans le dossier cible
+    if os.path.exists(destination):
+        print("Le fichier est déjà copié.")
+        return os.path.relpath(destination, os.getcwd())
+    
     # Copier le fichier dans le dossier cible
-    if file_path and target_folder:
-        filename = os.path.basename(file_path)
-        destination = os.path.join(target_folder, filename)
+    shutil.copyfile(file_path, destination)
+    
+    # Obtenir le chemin relatif du fichier copié par rapport au répertoire actuel
+    relative_path = os.path.relpath(destination, os.getcwd())
+    
+    return relative_path
 
-        if os.path.exists(destination):
-            return os.path.relpath(destination)
-            
-        else:
-            shutil.copyfile(file_path, destination)
-            return os.path.relpath(destination)
+
 
 def delete_file(file_path: str) -> str:
     """Supprime le fichier correspondant"""
