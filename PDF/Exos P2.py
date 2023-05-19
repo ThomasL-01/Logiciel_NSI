527f6277b0b6be49933e393466ef225e00892908
Thomas.L <90705601+ThomasL1G@users.noreply.github.com>
2023-05-19 16:59:29 +0200

    Mise à jour des fichiers -2023-05-19

---
+++ PDF/Exos P2.py
@@ -0,0 +1,16 @@
+from module import *
+#Exos P2:
+
+"""#ex 13 et 14
+
+def contient_double(t):
+    s = creer()
+    for x in t :
+        if contient(x,s):
+            return True
+        ajoute(s,x)
+    return False"""
+    
+#exo 15
+"""Les programmes 2, 4 
+"""
