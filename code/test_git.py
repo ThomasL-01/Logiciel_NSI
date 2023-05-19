cabd6dc12229d26883a166c4725e89fcc8880ebc
Thomas.L <90705601+ThomasL1G@users.noreply.github.com>
2023-05-19 17:28:32 +0200

    Encore une nouvelle modif

--- code/test_git.py
+++ code/test_git.py
@@ -59,20 +59,21 @@ def git_add_commit_push(directory_names: list, commit_message: list, branch: str
 def git_update(repo_path: str = os.getcwd(), branch: str = 'master', directories: list = None) -> None:
     """Pour l'utilisateur. Permet de mettre à jour sa version du logiciel avec tous les cours etc que l'admin a pu ajouter"""
     repo = Repo(repo_path)
-    
-    # Effectuer le pull pour récupérer les dernières modifications
     origin = repo.remotes.origin
-    origin.fetch()
     
     if directories is not None:
         for directory in directories:
             # Se déplacer vers le répertoire spécifié
             os.chdir(directory)
-            # Effectuer le pull dans le répertoire spécifié
-            repo.git.pull('origin', branch)
+            
+            # Vérifier si le répertoire est sale
+            repo_dir = Repo(os.getcwd())
+            if repo_dir.is_dirty():
+                # Réinitialiser le répertoire au commit de la branche distante
+                origin.fetch()
+                repo_dir.head.reset(commit=f'origin/{branch}', working_tree=True)
+                # Effectuer le pull pour récupérer les dernières modifications
+                origin.pull()
+            
             # Revenir au répertoire de départ
             os.chdir(repo_path)
-    else:
-        # Effectuer le pull sur l'ensemble du dépôt
-        origin.pull()
-
